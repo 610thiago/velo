7d7ac080d86e33ebd16d160865b6dc7ad67d65c2
Thiago Souza <610thiago@gmail.com>
2026-01-29 23:01:01 -0300

    voltando para o titulo original

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('a has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
