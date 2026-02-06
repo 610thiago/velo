cc52ee41637c5cca1f2ee0e791fe7bec960d9905
Thiago Souza <610thiago@gmail.com>
2026-02-05 22:40:52 -0300

    Desafio - Sobreviva sem Data Test IDs

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -6,28 +6,21 @@ import { test, expect } from '@playwright/test'
 test('deve consultar um pedido aprovado', async ({ page }) => {
   //Arrange
   await page.goto('http://localhost:5173/')
-
-  //Checkpoint 1: Verificar se a página de consulta de pedidos está acessível
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-  //Act
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  
-  //Checkpoints
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-EMP0B6')
-
-  //Act
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-EMP0B6')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-EMP0B6')
+  await expect(page.getByText('VLO-EMP0B6')).toBeVisible({timeout: 10000});
+  await expect(page.getByTestId('order-result-VLO-EMP0B6')).toContainText('VLO-EMP0B6');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByTestId('order-result-VLO-EMP0B6')).toContainText('APROVADO');
 
   
 })
