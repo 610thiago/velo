5c6fb2dc3ce8b45f5f86000e771f8458e836909f
Thiago Souza <610thiago@gmail.com>
2026-06-13 22:06:40 -0300

    build e deploy no vercel com secret de token correta

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-hn2omkm2a-610thiago-souza-s-projects1.vercel.app/',
+    baseURL: 'https://velo-610thiago-9836-610thiago-souza-s-projects1.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
