9365d6f814ec3861431dffbe2d2393caf23fc403
Thiago Souza <610thiago@gmail.com>
2026-06-13 23:28:21 -0300

    estagio de execução dos testes de regressão

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-610thiago-9836-610thiago-souza-s-projects1.vercel.app/',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
